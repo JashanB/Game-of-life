293e3d2fcd3a5619455843004e51bf93b93ed411
JashanB <jashan_brar50@hotmail.com>
2020-04-15 20:54:37 -0400

    setup file structure

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import './App.css';
+
+//set tile count to state (10 - then multiply by itself to form square)
+//set timer 
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tile/Tile.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from 'react'
+
+
+export default function Tile (props) {
+
+}
